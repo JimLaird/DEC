982b292ec3fef6321186af8c0bd04ee65e0d1910
JimLaird <jimlaird01@gmail.com>
2025-04-29 13:29:43 +0100

    added Nuget packages

--- DEC/MauiProgram.cs
+++ DEC/MauiProgram.cs
@@ -1,7 +1,18 @@
-using DEC.Services;
-using DEC.Shared.Services;
+using Blazored.LocalStorage;
+using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Database;
+using Firebase.Storage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Logging;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components.Icons;
+using DEC.Services;
+using DEC.Shared.Services;
+using DEC.Shared.Models;
+
 
 namespace DEC
 {
@@ -22,6 +33,23 @@ namespace DEC
 
             builder.Services.AddMauiBlazorWebView();
             builder.Services.AddFluentUIComponents();
+
+            // Add Firebase Auth configuration
+            var config = new FirebaseAuthConfig
+            {
+                ApiKey = Constants.FirebaseApiKey,
+                AuthDomain = Constants.FirebaseAuthDomain,
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                }
+            };
+
+            // Register FirebaseAuthClient
+            builder.Services.AddSingleton(new FirebaseAuthClient(config));
+
+            // Initialise Firebase Database
+            builder.Services.AddSingleton(new FirebaseClient("https://decouriers-6eeae-default-rtdb.europe-west1.firebasedatabase.app/"));
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
