da74adbac3e4e19f13681e8b28281816dcc44a35
JimLaird <jimlaird01@gmail.com>
2025-05-01 09:03:38 +0100

    Added File Upload and App Theming

---
+++ DEC.Shared/Services/IFirebaseStorageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Storage;
+
+namespace DEC.Shared.Services
+{
+    public interface IFirebaseStorageService
+    {
+        Task<string> UploadFile(Stream fileStream, string fileName);
+       
+    }
+}
---
+++ DEC/Services/FirebaseStorageService.cs
@@ -0,0 +1,32 @@
+﻿using Firebase.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DEC.Shared.Services;
+
+namespace DEC.Services
+{
+    public class FirebaseStorageService : IFirebaseStorageService
+    {
+        public async Task<string> UploadFile(Stream fileStream, string fileName)
+        {
+            var cancellation = new CancellationTokenSource();
+            var task = new FirebaseStorage(Constants.FirebaseStorageBucket).Child("uploads").Child(fileName)
+                .PutAsync(fileStream, cancellation.Token);
+
+            try
+            {
+                string downloadUrl = await task;
+                return downloadUrl;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error uploading file: {ex.Message}");
+                throw;
+            }
+        }
+
+    }
+}
