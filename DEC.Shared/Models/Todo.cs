99b10fcf1513120ae06cf5f3554b7624f240897b
JimLaird <jimlaird01@gmail.com>
2025-06-21 12:44:03 +0100

    implemented a task list

--- DEC.Shared/Models/Todo.cs
+++ DEC.Shared/Models/Todo.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DEC.Shared.Models
 {
-    internal class Todo
+    public class Todo
     {
+        public string Id { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Driver { get; set; }
+
+        public string Uid { get; set; }
     }
+
 }
